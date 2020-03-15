48664016de1ea734d64e0d138108ac91034ca02f
小羊 <tlylz99@live.cn>
2020-03-15 15:38:16 +0800

    添加项目文件。

---
+++ src/FileRepositoryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.FileProviders
+{
+    public static class FileRepositoryExtensions
+    {
+        public static async Task<string> ReadAsync(this IFileInfo file)
+        {
+            if (!file.Exists || file.IsDirectory)
+                return null;
+            using var fs = file.CreateReadStream();
+            using var sw = new StreamReader(fs);
+            return await sw.ReadToEndAsync();
+        }
+
+        public static async Task<byte[]> ReadBinaryAsync(this IFileInfo file)
+        {
+            if (!file.Exists || file.IsDirectory)
+                return null;
+            using var fs = file.CreateReadStream();
+            var result = new byte[file.Length];
+            await fs.ReadAsync(result, 0, result.Length);
+            return result;
+        }
+    }
+}
